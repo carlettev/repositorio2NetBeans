6da5e2a6f4bbfac659833a141c76433ccd2a0c1c
Usuario <Usuario@DESKTOP-N9D4KUR.ciclo.local>
2018-03-13 09:09:52 +0100

    hoolllaaa aa

--- src/pruebamarte13/Pruebamarte13.java
+++ src/pruebamarte13/Pruebamarte13.java
@@ -16,6 +16,7 @@ public class Pruebamarte13 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("hola mundo");
     }
     
 }
